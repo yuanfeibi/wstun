7c5d8e1df0c6bd1850385fbf1eb4e8161d6d729f
Nicola Peditto <npeditto@unime.it>
2017-10-20 12:26:59 +0200

    Added support to HTTPS for WST (no reverse).

--- bin/wst.js
+++ bin/wst.js
@@ -21,51 +21,82 @@ var portTunnel , argv, client, host, localport, optimist, port, server, wsHost, 
 
 var _ = require("under_score");
 
-optimist = require('optimist').usage("\nRun websocket tunnel and reverse tunnel such server or client.\nTo run websocket tunnel server: wstt.js -s 8080\nTo run websocket tunnel client: wstt.js -t localport:host:port ws://wshost:wsport\nNow connecting to localhost:localport is same as connecting to host:port on wshost\nIf websocket server is behind ssl proxy, then use \"wss://host:port\" in client mode\nFor security, you can \"lock\" the tunnel destination on server side, for eample:\nwstunnel -s 8080 -t host:port\nServer will tunnel incomming websocket connection to host:port only, so client can just run\n wstunnel -t localport ws://wshost:port\nIf client run:\n  wstunnel -t localpost:otherhost:otherport ws://wshost:port\n  * otherhost:otherport is ignored, tunnel destination is still \"host:port\" as specified on server.\n").string("s").string("t").alias('t', "tunnel").describe('s', 'run as server, specify listen port').describe('tunnel', 'run as tunnel client, specify localport:host:port');
+optimist = require('optimist').usage("\n" +
+    "Run websocket tunnel and reverse tunnel such server or client.\n" +
+    "To run websocket tunnel server: wstt.js -s 8080\nTo run websocket tunnel client: wstt.js -t localport:host:port ws://wshost:wsport\n" +
+    "Now connecting to localhost:localport is same as connecting to host:port on wshost\n" +
+    "If websocket server is behind ssl proxy, then use \"wss://host:port\" in client mode\n" +
+    "For security, you can \"lock\" the tunnel destination on server side, for eample:\n" +
+    "wstunnel -s 8080 -t host:port\n" +
+    "Server will tunnel incomming websocket connection to host:port only, so client can just run\n" +
+    "wstunnel -t localport ws://wshost:port\n" +
+    "If client run:\n" +
+    "wstunnel -t localpost:otherhost:otherport ws://wshost:port\n" +
+    "* otherhost:otherport is ignored, tunnel destination is still \"host:port\" as specified on server.\n"
+).string("s").describe('s', 'run as server, specify listen port')
+    .string("t").alias('t', "tunnel").describe('tunnel', 'run as tunnel client, specify localport:host:port')
+    .string("ssl").describe('ssl', '\"true\" | \"false\" to enable|disable HTTPS communication.')
+    .string("key").describe('key', '[only with --ssl=true] path to private key certificate.')
+    .string("cert").describe('cert', '[only with --ssl=true] path to public key certificate.');
 
+argv = optimist.argv;
 
-  argv = optimist.argv;
+wst = require("../lib/wst");
 
-  if (_.size(argv) === 2) {
-    return console.log(optimist.help());
+if (argv.s && !argv.r) {
+  
+  // WS tunnel server side
+  if (argv.t) {
+    _ref = argv.t.split(":"), host = _ref[0], port = _ref[1];
+    //server = new wst.server(host, port, argv.ssl, argv.key, argv.cert);
+    server = new wst.server({dstHost:dstHost, dstPort:dstPort, ssl:https_flag, key:key, cert:cert});
+  }
+  else {
+    server = new wst.server({ssl:argv.ssl, key:argv.key, cert:argv.cert});
   }
 
-  if (argv.s && !argv.r) {
-    wst = require("../lib/wst");
-    if (argv.t) {
-      _ref = argv.t.split(":"), host = _ref[0], port = _ref[1];
-      server = new wst.server(host, port);
-    } 
-    else {
-      server = new wst.server;
-    }
-    server.start(argv.s);
-  }else if (argv.t) {
-    require("../lib/https_override");
-    wst = require("../lib/wst");
-    client = new wst.client;
-    wsHost = _.last(argv._);
-    _ref1 = argv.t.split(":"), localport = _ref1[0], host = _ref1[1], port = _ref1[2];
-    if (host && port) {
-      client.start(localport, wsHost, "" + host + ":" + port);
-    } else {
-      client.start(localport, wsHost);
-    }
-  }else if (argv.r) {
-    if (argv.s){
-      wst = require("../lib/wst");
-      server = new wst.server_reverse(argv.x, argv.key, argv.cert);
-      server.start(argv.s);
-    }
-    else{
-      wst = require("../lib/wst");
-      client = new wst.client_reverse;
-      wsHost = _.last(argv._);
-      var test = argv.r.split(":");
-      _ref1 = argv.r.split(":"), portTunnel = _ref1[0], host = _ref1[1], port =_ref1[2];
-      client.start(portTunnel, wsHost, "" + host + ":" + port);
-      
-    } 
+  server.start(argv.s);
+
+}else if (argv.t) {
+
+  // WS tunnel client side
+
+  client = new wst.client;
+
+  wsHost = _.last(argv._);
+  _ref1 = argv.t.split(":"), localport = _ref1[0], host = _ref1[1], port = _ref1[2];
+
+  if (host && port) {
+    client.start(localport, wsHost, "" + host + ":" + port);
   } else {
-    return console.log(optimist.help());
+    client.start(localport, wsHost);
   }
+
+
+}else if (argv.r) {
+
+  // WS reverse tunnel
+
+  if (argv.s){
+
+    // Server side
+    server = new wst.server_reverse({ssl:argv.ssl, key:argv.key, cert:argv.cert});
+    server.start(argv.s);
+
+  }
+  else{
+
+    // Client side
+    client = new wst.client_reverse;
+    wsHost = _.last(argv._);
+    _ref1 = argv.r.split(":"), portTunnel = _ref1[0], host = _ref1[1], port =_ref1[2];
+    client.start(portTunnel, wsHost, "" + host + ":" + port);
+
+  }
+
+} else {
+
+  // Wrong options
+  return console.log(optimist.help());
+
+}
